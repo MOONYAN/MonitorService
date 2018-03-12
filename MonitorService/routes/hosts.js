add20b020ba9b582d6f843fd5d5d234292bb8cc8
MOONYAN <1787hz@gmail.com>
2018-03-12 21:12:26 +0800

    hostConfig

--- MonitorService/routes/hosts.js
+++ MonitorService/routes/hosts.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var nmap = require('node-nmap');
+var hosts = require('../config/host');
 
 router.post('/', (req, res) => {
     console.log(req.body);
@@ -36,32 +37,6 @@ router.post('/', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    let hosts = [
-        {
-            id: 1,
-            name: 'localhost',
-            ip: '127.0.0.1',
-            status: 'Unknown'
-        },
-        {
-            id: 2,
-            name: 'google.com.tw',
-            ip: '172.217.160.99',
-            status: 'Unknown'
-        },
-        {
-            id: 3,
-            name: 'facebook.com',
-            ip: '31.13.87.36',
-            status: 'Unknown'
-        },
-        {
-            id: 4,
-            name: 'youtube.com',
-            ip: '216.58.200.46',
-            status: 'Unknown'
-        }
-    ];
     res.json( hosts );
 });
 
