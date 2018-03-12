fbcb69e578fff886dc74a2e8e4077fe73dd4666b
MOONYAN <1787hz@gmail.com>
2018-03-12 21:05:24 +0800

    static hosts

--- MonitorService/routes/hosts.js
+++ MonitorService/routes/hosts.js
@@ -41,13 +41,25 @@ router.get('/', (req, res) => {
             id: 1,
             name: 'localhost',
             ip: '127.0.0.1',
-            status: 'UP'
+            status: 'Unknown'
         },
         {
             id: 2,
             name: 'google.com.tw',
             ip: '172.217.160.99',
-            status: 'UP'
+            status: 'Unknown'
+        },
+        {
+            id: 3,
+            name: 'facebook.com',
+            ip: '31.13.87.36',
+            status: 'Unknown'
+        },
+        {
+            id: 4,
+            name: 'youtube.com',
+            ip: '216.58.200.46',
+            status: 'Unknown'
         }
     ];
     res.json( hosts );
